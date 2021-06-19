8951bc66df4faf7c2d2d09e4915d61018d825bd8
mjuc <michal.jucewicz@protonmail.com>
2021-06-19 20:53:07 +0200

    Added drawing of simulated population. Added comments describing sections of code.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 import thorpy
 from specimen import Specimen
+from time import sleep
+import matplotlib.pyplot as plt
 
 WHITE = (255, 255, 255)
-GREEN = (  0, 255,   0)
-RED = (255,   0,   0)
-YELLOW = (255,255,0)
-ORANGE = (255,140,0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 140, 0)
 
 #simulation parameters
 population=[]
-population_size=100
+population_size=1000
 simulation_length=100
+simulate_flag=True
 
 #custom pygame event responsible for starting simulation
 run_sim_event = pygame.event.Event(pygame.USEREVENT+1)
@@ -30,7 +33,7 @@ def set_params():
 def create_pop():
     for i in range(population_size):
         temp=Specimen()
-        temp.setCoordinates()
+        temp.setCoordinates(screen_x=700,screen_y=700)
         temp.setSpeed()
         population.append(temp)
         del(temp)
@@ -42,12 +45,13 @@ screen.fill(WHITE)
 
 #menu
 #sliders
-slider_size = thorpy.SliderX(400, (100, 500), "Initial population(default 100)")
+slider_size = thorpy.SliderX(500, (1000, 1500), "Initial population(default 1000)")
 slider_length = thorpy.SliderX(400, (100,500), "Simulation length(default 100)")
 #buttons
 run_sim_button = thorpy.make_button("Run simulation",func=run_sim)
 set_params_button = thorpy.make_button("Set parameters values",func=set_params)
 quit_button = thorpy.make_button("Quit", func=thorpy.functions.quit_func)
+#main menu
 box=thorpy.Box(elements=[slider_size,
                          slider_length,
                          set_params_button,
@@ -65,11 +69,55 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.USEREVENT+1:
-            pass
+            #create population according to set parameters
+            create_pop()
+            screen.fill(WHITE)
+            for i in range(simulation_length):
+                screen.fill(WHITE)
+                #draw population
+                for p in population:
+                    if p.state=='C':
+                        #sick specimen as red pixel
+                        pygame.draw.circle(screen,RED,(p.x,p.y),1)
+                    elif p.state=='Z':
+                        #infected specimen as yellow pixel
+                        pygame.draw.circle(screen,YELLOW,(p.x,p.y),1)
+                    elif p.state=='ZD':
+                        #recowering specimen as orange pixel
+                        pygame.draw.circle(screen,ORANGE,(p.x,p.y),1)
+                    elif p.state=='ZZ':
+                        #healthy specimen as green pixel
+                        pygame.draw.circle(screen,GREEN,(p.x,p.y),1)
+                pygame.display.update()
+                #for next turn
+                for p in population:
+                    #check if move will result in hitting wall
+                    p.checkWall(700,700)
+                    #move specimen
+                    p.move()
+                    #check if move resulted in any interactions
+                    children=p.checkContact(population)
+                    #add potential children to population
+                    if len(children)>0:
+                        for c in children:
+                            population.append(c)
+                    #increment age of specimen and ammend parameters as required
+                    p.getOlder()
+                for p in population:
+                    #remove dead specimen from population
+                    if p.alive==False:
+                        population.remove(p)
+                    #clean history of interactions
+                    p.clean()
+                #wait for 0.25s to make each turn visible
+                sleep(0.25)
+            simulate_flag=False
         else:
+            #print menu
             screen.fill(WHITE)
             box.blit()
             box.update()
+            menu.react(event)
             pygame.display.update()
-        menu.react(event)
-        pygame.display.update()
+
+pygame.quit()
